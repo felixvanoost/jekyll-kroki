c03bc4ce166a9fde1f9d6de23d8be9f661bc8da6
Felix van Oost <felix.vanoost@live.com>
2023-10-20 17:50:05 -0400

    Add basic unit tests for kroki_url()

--- test/jekyll/test_kroki.rb
+++ test/jekyll/test_kroki.rb
@@ -7,5 +7,27 @@ module Jekyll
     def test_that_it_has_a_version_number
       refute_nil ::Jekyll::Kroki::VERSION
     end
+
+    def test_valid_kroki_url
+      url = "https://rubygems.org/"
+      config = { "jekyll-kroki" => { "kroki_url" => url } }
+      assert_equal ::Jekyll::Kroki.kroki_url(config), URI(url)
+    end
+
+    def test_invalid_kroki_url
+      url = "ht//rubygems.org/"
+      config = { "jekyll-kroki" => { "kroki_url" => url } }
+      assert_raises(TypeError) { ::Jekyll::Kroki.kroki_url(config) }
+    end
+
+    def test_missing_kroki_url
+      config = { "jekyll-kroki" => { "pi" => 3.14 } }
+      assert_equal ::Jekyll::Kroki.kroki_url(config), URI("https://kroki.io")
+    end
+
+    def test_missing_kroki_config
+      config = { "jekyll-another-plugin" => { "pi" => 3.14 } }
+      assert_equal ::Jekyll::Kroki.kroki_url(config), URI("https://kroki.io")
+    end
   end
 end
